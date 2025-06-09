9c03ed3bfad7e8c03d6f1adb7c002889e01158b9
Medonca09 <lucasmm0908@gmail.com>
2025-06-09 11:04:25 -0300

    feat: criando novos componentes.

---
+++ src/app/shared/components/body-home/body-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body-home',
+  standalone: false,
+  templateUrl: './body-home.component.html',
+  styleUrl: './body-home.component.scss'
+})
+export class BodyHomeComponent {
+
+}
