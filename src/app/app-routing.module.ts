dc19eded532c5568c1b734fc67cd0dd2ea3da180
Medonca09 <lucasmm0908@gmail.com>
2025-06-09 13:45:23 -0300

    feat: iniciando o caminho das rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyHomeComponent } from './shared/components/body-home/body-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BodyHomeComponent},
+  {path: 'home', component: BodyHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
