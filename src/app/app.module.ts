3bb2c3d6c73add08b1c338be911407003a8158d3
Medonca09 <lucasmm0908@gmail.com>
2025-06-09 13:47:30 -0300

    feat: importando components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyHomeComponent } from './shared/components/body-home/body-home.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BodyHomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
