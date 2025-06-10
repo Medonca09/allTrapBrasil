49994578aa9834c563a4477571268766b78824f6
Medonca09 <lucasmm0908@gmail.com>
2025-06-10 11:20:14 -0300

    feat: adicionando novas rotas das páginas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyHomeComponent } from './shared/components/body-home/body-home.component';
+import { BodyAboutComponent } from './shared/components/body-about/body-about.component';
 
 const routes: Routes = [
   {path: '', component: BodyHomeComponent},
-  {path: 'home', component: BodyHomeComponent}
+  {path: 'home', component: BodyHomeComponent},
+  {path: 'sobre', component: BodyAboutComponent}
 ];
 
 @NgModule({
